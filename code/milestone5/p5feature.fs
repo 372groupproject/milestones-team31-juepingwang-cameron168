8b8a90fb8a2d705d75b3ebd5d44818f4aea339e5
juepingwang <46639865+juepingwang@users.noreply.github.com>
2020-04-27 20:51:23 -0700

    milestone5

---
+++ code/milestone5/p5feature.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+
+let mySqrt num = 
+    match num with
+        | num when num <= 0.0 -> None
+        | _ -> Some(sqrt num)
+
+let sqrtList mySqrt list =
+    list 
+    |> List.choose (fun x -> (mySqrt x))
+
+let numbers = [25.0; -4.0; -1.0; 4.0; -8.0; 9.0;]
+
+printfn "Original List: %A"  numbers
+printfn "SqrtList: %A"  (sqrtList mySqrt numbers)
